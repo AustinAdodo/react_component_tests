4dd252bcb391d5a2330954a2fe6af237007bb7de
Odia Adodo <subzbelow@gmail.com>
2023-11-21 23:01:23 +0100

    Dynamic response

--- __tests__/dynamic.test.js
+++ __tests__/dynamic.test.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 describe("candidate written response", () => {
   it("should be modified", done => {
     const originalLength = 391;
-    fs.readFile("./candidate-written-response.md", (err, data) => {
+    fs.readFile("./dynamic.md", (err, data) => {
       expect(data.length).toBeGreaterThan(originalLength);
       done();
     });
---
+++ dynamic.md
@@ -0,0 +1,6 @@
+#Suggestions.
+
+To improve performance in the search app if the inventory contained millions of titles.
+The data be stored as the app runs, and how can it be retrieved quickly? 
+
+#Trade-offs and design decisions.
