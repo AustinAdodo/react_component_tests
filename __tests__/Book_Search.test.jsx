af328d220d72452b18ccd382f9dfd42cbbb6fae7
Odia Adodo <subzbelow@gmail.com>
2023-11-21 22:48:47 +0100

    update

--- __tests__/Book_Search.test.jsx
+++ __tests__/Book_Search.test.jsx
@@ -10,6 +10,16 @@ import {
 } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import BookSearch from "../src/components/BookSearch";
+
+/**
+* DOM Testing Library (and other Testing Library frameworks built on top of it) exports 
+* a screen object which has every query (and a debug method). This works better with autocomplete 
+* and makes each test a little simpler to write and maintain.
+* @see https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/prefer-screen-queries.md
+* @params
+*
+*/
+
 const {readFile} = promises;
 const {
   getAllByTestId,
